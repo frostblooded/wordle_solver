d55641fa344df9b23e7e74b2c54ad2c87640df0a
Nikolay Danailov <frostblooded@yahoo.com>
2023-09-08 12:39:33 +0300

    Remove data converter

---
+++ src/config.rs
@@ -0,0 +1,2 @@
+pub const INPUT_FILE_PATH: &str = "resources/words.txt";
+pub const WORD_LENGTH: usize = 5;
--- src/solver/bin/config.rs
+++
@@ -1,2 +0,0 @@
-pub const INPUT_FILE_PATH: &str = "resources/words_5_chars.txt";
-pub const WORD_LENGTH: usize = 5;
